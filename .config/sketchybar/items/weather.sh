2a0c38ca7b4de52e4db4befdc47ccc0633795674
Qin Wang <wqin20121@163.com>
2025-10-02 18:51:32 +0800

    updates

--- .config/sketchybar/items/weather.sh
+++ .config/sketchybar/items/weather.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+weather=(
+    width=70
+    align=right
+    padding_left=0
+    padding_right=0
+    label.padding_right=0
+    label="--°"
+    update_freq=300
+
+    # Of the 3 data sources, select the one you find most accurate.
+    script="$PLUGIN_DIR/weather_baidu.sh"
+    # script="$PLUGIN_DIR/weather_nmc.sh"
+    # script="$PLUGIN_DIR/weather_meteo.sh"
+)
+
 sketchybar --add item weather left \
-  --set weather \
-  update_freq=600 \
-  script="$PLUGIN_DIR/weather.sh" \
-  icon.font="Hack Nerd Font:Bold:17.0"
+    --set weather "${weather[@]}" \
+    #--subscribe weather mouse.clicked
